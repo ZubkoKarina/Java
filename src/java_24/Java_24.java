475baa709792a6c5241ab4a0253d78ccdfecc052
Zubko Karina <karinazubko2017@gmail.com>
2022-10-27 23:31:38 +0300

    ready program

---
+++ src/java_24/Java_24.java
@@ -0,0 +1,18 @@
+
+package java_24;
+
+/**
+ *
+ * @author karina
+ */
+
+public class Java_24 {
+
+    public static void main(String[] args) {
+        String greeting = "Hello";
+        System.out.println(greeting);
+    }
+    
+    
+    
+}
---
+++ src/java_24/Root.java
@@ -0,0 +1,12 @@
+package java_24;
+
+class Root {
+    public static void main(String[] arguments){
+        int number = 225;
+        System.out.println("Квадратний корень из "
+            + number
+            + " равен "
+            + Math.sqrt(number)
+        );
+    }
+}
