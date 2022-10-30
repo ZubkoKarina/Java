de06ae90eeab604e84f7f5703130bdb9d5f40603
Zubko Karina <karinazubko2017@gmail.com>
2022-10-30 21:34:02 +0200

    Gremlins

--- src/java_24/Gremlin.java
+++ src/java_24/Gremlin.java
@@ -3,6 +3,7 @@ package java_24;
 
 public class Gremlin{
     static int gremlinCount = 0;
+    private int guid;
     
     public Gremlin(){
         gremlinCount++;
@@ -10,5 +11,15 @@ public class Gremlin{
     
     static int getGremlinCount(){
         return gremlinCount;
+    }
+    
+    public int getGuid(){
+        return guid;
+    }
+    
+    public void setGuid(int newGuid) {
+        if (newGuid > 999_999 & newGuid < 10_000_000) {
+            guid = newGuid;
+        }
     }
 }
