8357586b87488d3e2037ed01ccfba648debabdfd
Zubko Karina <karinazubko2017@gmail.com>
2022-10-30 20:47:13 +0200

    Gremlins

---
+++ src/java_24/Gremlin.java
@@ -0,0 +1,14 @@
+
+package java_24;
+
+public class Gremlin{
+    static int gremlinCount = 0;
+    
+    public Gremlin(){
+        gremlinCount++;
+    }
+    
+    static int getGremlinCount(){
+        return gremlinCount;
+    }
+}
